2ce1499368e9fa9d32d22b18402db1a1c7534377
Irina Belchikova <iboronenkova@gmail.com>
2020-10-03 22:21:27 +0300

    Added hamburger menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 import '../styles/index.css';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('.header');
+  document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('header__hamburger')) {
+      header.classList.toggle('header_visible-mobile-menu');
+      if (header.classList.contains('header_visible-mobile-menu')) {
+        document.querySelector('.header__menu').style.top = `${header.clientHeight}px`;
+      }
+    } else {
+      header.classList.remove('header_visible-mobile-menu');
+    }
+  });
+});
