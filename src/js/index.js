d808660a2c22787842c876eba279aca6d0d2bc00
Irina Belchikova <iboronenkova@gmail.com>
2020-10-05 22:25:15 +0300

    scripts

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,13 @@
 import '../styles/index.css';
 import './hamburger';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const foundMoreResults = document.querySelector('.found__more-results');
+  foundMoreResults.addEventListener('click', () => {
+    foundMoreResults.style.display = 'none';
+  });
+
+  document.querySelector('.search__bar').addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
+});
