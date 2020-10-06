cfca12e7eea9484421dcea501e8ee30095d13c21
Irina Belchikova <iboronenkova@gmail.com>
2020-10-06 22:35:16 +0300

    some js, changed editorconfig

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,10 @@ import './hamburger';
 window.addEventListener('DOMContentLoaded', () => {
   const foundMoreResults = document.querySelector('.found__more-results');
   foundMoreResults.addEventListener('click', () => {
+    const cards = document.querySelector('.cards');
+    document.querySelectorAll('.cards .card').forEach((card) => {
+      cards.appendChild(card.cloneNode(true));
+    });
     foundMoreResults.style.display = 'none';
   });
 
@@ -14,6 +18,15 @@ window.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('click', (e) => {
     if (e.target.classList.contains('card__bookmark')) {
       e.target.classList.toggle('card__bookmark_marked');
+    }
+  });
+
+  const userButton = document.querySelector('.header__menu-user');
+  userButton.addEventListener('click', () => {
+    if (userButton.classList.contains('header__menu-user_signedin')) {
+      userButton.classList.remove('header__menu-user_signedin');
+      document.querySelector('.header__user-text').textContent = 'Авторизоваться';
+      document.querySelectorAll('.header__menu li')[1].style.display = 'none';
     }
   });
 });
