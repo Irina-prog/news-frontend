117c28691abddaadd75aa269105dc5364e00e6f2
Irina Belchikova <iboronenkova@gmail.com>
2020-10-07 20:14:39 +0300

    browserlistrc

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,8 @@ const presets = [
   [
     '@babel/env',
     {
-      targets: {
-        browsers: [
-          'last 5 versions',
-          'edge 15',
-        ],
-      },
-      useBuiltIns: 'usage', // эта настройка babel-polyfill, если стоит значение usage, то будут подставлятся полифилы для версий браузеров которые указали ниже.
-      corejs: '3.4.1', // явно проставить версию corejs
+      useBuiltIns: 'usage',
+      corejs: '3.4.1',
     },
   ],
 ];
