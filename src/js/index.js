1da4d0490e691848fa8c51820aebc350b11d8390
Irina Belchikova <iboronenkova@gmail.com>
2020-10-08 18:24:10 +0300

    popups

--- src/js/index.js
+++ src/js/index.js
@@ -15,6 +15,10 @@ window.addEventListener('DOMContentLoaded', () => {
   const notFound = document.querySelector('.notfound');
   const found = document.querySelector('.found');
 
+  const registerPopup = document.querySelector('#register');
+  const loginPopup = document.querySelector('#login');
+  const registeredPopup = document.querySelector('#registered');
+
   preloader.style.display = 'none';
   notFound.style.display = 'none';
   found.style.display = 'none';
@@ -39,6 +43,9 @@ window.addEventListener('DOMContentLoaded', () => {
     if (e.target.classList.contains('card__bookmark')) {
       e.target.classList.toggle('card__bookmark_marked');
     }
+    if (e.target.classList.contains('popup__close')) {
+      e.target.closest('.popup').classList.remove('popup_is-opened');
+    }
   });
 
   const userButton = document.querySelector('.header__menu-user');
@@ -47,6 +54,42 @@ window.addEventListener('DOMContentLoaded', () => {
       userButton.classList.remove('header__menu-user_signedin');
       document.querySelector('.header__user-text').textContent = 'Авторизоваться';
       document.querySelectorAll('.header__menu li')[1].style.display = 'none';
+    } else {
+      loginPopup.classList.add('popup_is-opened');
     }
   });
+
+  registerPopup.querySelector('.popup__link').addEventListener('click', (e) => {
+    e.preventDefault();
+    loginPopup.classList.add('popup_is-opened');
+    registerPopup.classList.remove('popup_is-opened');
+  });
+
+  loginPopup.querySelector('.popup__link').addEventListener('click', (e) => {
+    e.preventDefault();
+    loginPopup.classList.remove('popup_is-opened');
+    registerPopup.classList.add('popup_is-opened');
+  });
+
+  registeredPopup.querySelector('.popup__link').addEventListener('click', (e) => {
+    e.preventDefault();
+    loginPopup.classList.add('popup_is-opened');
+    registeredPopup.classList.remove('popup_is-opened');
+  });
+
+  registerPopup.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    registerPopup.classList.remove('popup_is-opened');
+    registeredPopup.classList.add('popup_is-opened');
+  });
+
+  loginPopup.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    loginPopup.classList.remove('popup_is-opened');
+    userButton.classList.add('header__menu-user_signedin');
+    document.querySelector('.header__user-text').textContent = 'Грета';
+    document.querySelectorAll('.header__menu li')[1].style.display = '';
+  });
+
+  document.querySelectorAll('.header__menu li')[1].style.display = 'none';
 });
