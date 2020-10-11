b9f6defb0d3a6ed38039afed00d2e66548080936
Irina Belchikova <iboronenkova@gmail.com>
2020-10-11 11:01:28 +0300

    fixes in scripts

--- src/js/bookmarks.js
+++ src/js/bookmarks.js
@@ -6,4 +6,7 @@ document.addEventListener('click', (e) => {
   if (e.target.classList.contains('card__button_trash')) {
     e.target.closest('.card').remove();
   }
+  if (e.target.closest('.header__menu-user')) {
+    window.location.href = '/';
+  }
 });
