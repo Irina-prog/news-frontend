0ef7828bcb1915e4a1ccf7b60abf095e2ad97054
Irina Belchikova <iboronenkova@gmail.com>
2020-10-20 18:06:51 +0300

    Adeed insertAdjacentHTML

--- src/js/components/summary.js
+++ src/js/components/summary.js
@@ -6,6 +6,8 @@ export default class Summary extends Component {
   setSummary({ name }, cardList) {
     this._element.querySelector('.summary__title').textContent = `${name}, у вас ${cardList.length} ${savedArticles(cardList.length)}`;
     const topKeywords = getOrderedKeywordsByUsage(cardList);
-    this._element.querySelector('.summary__tags').innerHTML = topKeywords.length > 0 ? `По ключевым словам: ${formatKeywords(topKeywords)}` : '';
+    const tags = this._element.querySelector('.summary__tags');
+    tags.innerHTML = '';
+    tags.insertAdjacentHTML('beforeend', topKeywords.length > 0 ? `По ключевым словам: ${formatKeywords(topKeywords)}` : '');
   }
 }
